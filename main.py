c04ee89cd1a7f4881baca317711fa2e8a8d8276c
Suparno Pal <pal.suparno2001@gmail.com>
2021-04-29 16:58:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import smtplib
 server = smtplib.SMTP('smtp.gmail.com', 587)
 server.starttls()
-server.login('worldomedia1@gmail.com', '9874792606')
-server.sendmail('worldomedia1@gmail.com',
-                'pal.suparno2001@gmail.com',
+server.login('my email id', 'my password')
+server.sendmail('my email id',
+                'email id of reciever',
                 'Hey Suparno! How are you you doing? Hope to see you soon as I am going there soon')
